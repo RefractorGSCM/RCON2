8829bb0310d7ac93c15f4c84b4db22c7ec24e337
dsnidr <6261352+dsnidr@users.noreply.github.com>
2022-01-30 18:24:28 -0500

    Packet encoding and decoding

--- packet/packet.go
+++ packet/packet.go
@@ -1,23 +1,31 @@
 package packet
 
 import (
+	"bytes"
 	"encoding/binary"
+	"io"
 	"math"
 
+	"github.com/pkg/errors"
 	"go.uber.org/atomic"
 )
 
+type PacketHeader struct {
+	Size int32
+	ID int32
+	Type PacketType
+}
 type Packet struct {
-	mode binary.ByteOrder
-	pType PacketType
-	body []byte
-	id int32
+	PacketHeader
+
+	Mode binary.ByteOrder
+	Body []byte
 }
 
 var nextClientPacketID = atomic.Int32{}
 
 func init() {
-	nextClientPacketID.Store(1)
+	nextClientPacketID.Store(0)
 }
 
 func idInArr(arr []int32, id atomic.Int32) bool {
@@ -30,7 +38,7 @@ func idInArr(arr []int32, id atomic.Int32) bool {
 	return false
 }
 
-func getNextID(restrictedIDs []int32) int32 {
+func nextID(restrictedIDs []int32) int32 {
 	if nextClientPacketID.Load()+1 == math.MaxInt32 {
 		nextClientPacketID.Store(1)
 	} else {
@@ -47,4 +55,83 @@ func getNextID(restrictedIDs []int32) int32 {
 	}
 
 	return nextClientPacketID.Load()
+}
+
+const (
+	headerBytes = 8
+	padBytes = 2
+)
+
+func New(mode binary.ByteOrder, pType PacketType, body []byte, restrictedIDs []int32) *Packet {
+	id := nextID(restrictedIDs)
+
+	p := &Packet{
+		PacketHeader: PacketHeader{
+			ID: id,
+			Type: pType,
+			Size: int32(len(body) + headerBytes + padBytes),
+		},
+		Mode: mode,
+		Body: body,
+	}
+
+	if len(body) == 0 {
+		p.Body = []byte{}
+	}
+
+	return p
+}
+
+func (p *Packet) Encode() ([]byte, error) {
+	buffer := bytes.NewBuffer([]byte{})
+
+	order := p.Mode
+
+	if err := binary.Write(buffer, order, p.Size); err != nil {
+		return nil, errors.Wrap(err, "could not write packet size")
+	}
+
+	if err := binary.Write(buffer, order, p.ID); err != nil {
+		return nil, errors.Wrap(err, "could not write packet id")
+	}
+
+	if err := binary.Write(buffer, order, p.Type); err != nil {
+		return nil, errors.Wrap(err, "could not write packet type")
+	}
+
+	if err := binary.Write(buffer, order, p.Body); err != nil {
+		return nil, errors.Wrap(err, "could not write packet body")
+	}
+
+	if err := binary.Write(buffer, order, [2]byte{}); err != nil {
+		return nil, errors.Wrap(err, "could not write packet padding")
+	}
+
+	return buffer.Bytes(), nil
+}
+
+var malformedPacketErr = errors.New("malformed packet")
+var badPacketTypeErr = errors.New("bad packet type")
+
+func Decode(mode binary.ByteOrder, reader io.Reader) (*Packet, error) {
+	header := PacketHeader{}
+
+	if err := binary.Read(reader, mode, &header); err != nil {
+		return nil, errors.Wrap(err, malformedPacketErr.Error())
+	}
+
+	payload := make([]byte, header.Size - headerBytes)
+	_, err := io.ReadFull(reader, payload)
+	if err != nil {
+		return nil, errors.Wrap(err, malformedPacketErr.Error())
+	}
+
+	if header.Type != TypeAuthRes && header.Type != TypeCommandRes {
+		return nil, badPacketTypeErr
+	}
+
+	return &Packet{
+		PacketHeader: header,
+		Body: payload[:len(payload)-2],
+	}, nil
 }
--- packet/packet_test.go
+++ packet/packet_test.go
@@ -1,6 +1,8 @@
 package packet
 
 import (
+	"bytes"
+	"encoding/binary"
 	"math"
 	"testing"
 
@@ -25,18 +27,77 @@ func Test_getNextID(t *testing.T) {
 	restrictedIDs := []int32{1000, 1001, 1002, 1003, 1005, 1006, 1007, 1008, 1009}
 
 	// Test correct incrementing
-	assert.Equal(t, int32(2), getNextID(restrictedIDs))
-	assert.Equal(t, int32(3), getNextID(restrictedIDs))
-	assert.Equal(t, int32(4), getNextID(restrictedIDs))
-	assert.Equal(t, int32(5), getNextID(restrictedIDs))
+	assert.Equal(t, int32(2), nextID(restrictedIDs))
+	assert.Equal(t, int32(3), nextID(restrictedIDs))
+	assert.Equal(t, int32(4), nextID(restrictedIDs))
+	assert.Equal(t, int32(5), nextID(restrictedIDs))
 
 	// Test max int value reset
 	nextClientPacketID.Store(math.MaxInt32-1)
-	assert.Equal(t, int32(1), getNextID(restrictedIDs))
+	assert.Equal(t, int32(1), nextID(restrictedIDs))
 
 	// Test skipping restricted values
 	nextClientPacketID.Store(999)
-	assert.Equal(t, int32(1004), getNextID(restrictedIDs))
-	assert.Equal(t, int32(1010), getNextID(restrictedIDs))
-	assert.Equal(t, int32(1011), getNextID(restrictedIDs))
+	assert.Equal(t, int32(1004), nextID(restrictedIDs))
+	assert.Equal(t, int32(1010), nextID(restrictedIDs))
+	assert.Equal(t, int32(1011), nextID(restrictedIDs))
+}
+
+func TestNew(t *testing.T) {
+	mode := binary.LittleEndian
+	pType := TypeCommand
+	body := []byte("hello world!")
+
+	nextClientPacketID.Store(0)
+	packet := New(mode, pType, body, nil)
+	assert.Equal(t, int32(1), packet.ID)
+	assert.Equal(t, pType, packet.Type)
+	assert.Equal(t, mode, packet.Mode)
+	assert.Equal(t, body, packet.Body)
+}
+
+func TestPacketBuildAndDecode(t *testing.T) {
+	const expectedBody = "test command string"
+	const expectedSize = int32(len(expectedBody) + headerBytes + padBytes)
+	const expectedID = int32(10)
+	const expectedType = int32(TypeCommand)
+
+	nextClientPacketID.Store(expectedID - 1)
+	packet := New(binary.LittleEndian, TypeCommand, []byte(expectedBody), []int32{})
+
+	// 1. Test packet build
+	out, err := packet.Encode()
+	assert.Nil(t, err)
+
+	buffer := bytes.NewBuffer(out)
+	
+	var size int32
+	var id int32
+	var pType int32
+
+	// 1.5 Check packet fields
+	err = binary.Read(buffer, packet.Mode, &size)
+	assert.Nil(t, err)
+	assert.Equal(t, expectedSize, size)
+
+	err = binary.Read(buffer, packet.Mode, &id)
+	assert.Nil(t, err)
+	assert.Equal(t, expectedID, id)
+
+	err = binary.Read(buffer, packet.Mode, &pType)
+	assert.Nil(t, err)
+	assert.Equal(t, expectedType, pType)
+
+	// 2. Test packet decode
+	out, err = packet.Encode()
+	assert.Nil(t, err)
+	buffer = bytes.NewBuffer(out)
+
+	packet, err = Decode(packet.Mode, buffer)
+	assert.Nil(t, err)
+
+	assert.Equal(t, expectedSize, packet.Size)
+	assert.Equal(t, expectedID, packet.ID)
+	assert.Equal(t, expectedType, int32(packet.Type))
+	assert.Equal(t, expectedBody, string(packet.Body))
 }
